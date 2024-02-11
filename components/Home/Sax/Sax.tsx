095f568592ba6a1286dfcd0175f2c0fa2e8d8e5e
coder6583 <thedude6583@gmail.com>
2024-02-11 07:39:05 -0500

    sax

--- components/Home/Sax/Sax.tsx
+++ components/Home/Sax/Sax.tsx
@@ -1,10 +1,38 @@
+import { Image } from "@chakra-ui/react";
 import SectionHeader from "../SectionHeader";
 import styles from "./Sax.module.css";
+import Link from "next/link";
+import { ChevronRightIcon } from "@chakra-ui/icons";
 
 export default function Sax(): JSX.Element {
   return (
     <div className={styles.parent}>
       <SectionHeader header="JAZZ" subheader="ジャズ" />
+      <div className={styles.gallery}>
+        <div className={styles.column}>
+          <div className={styles.image_tall}>
+            <Image
+              src="/images/yumi_sax_standing.png"
+              alt="sax_standing"
+              height={"100%"}
+            />
+          </div>
+        </div>
+        <div className={styles.column}>
+          <div className={styles.image}>
+            <Image src="/images/yumi_flute.jpg" alt="yumi_flute" />
+          </div>
+          <div className={styles.image}>
+            <Image src="/images/yumi_sax.jpg" alt="yumi_sax" />
+          </div>
+        </div>
+      </div>
+      <Link href="/sax" className={styles.link}>
+        <div className={styles.words}>SAXを見る</div>
+        <div className={styles.icon}>
+          <ChevronRightIcon fontSize={24} marginBottom={1} />
+        </div>
+      </Link>
     </div>
   );
 }
