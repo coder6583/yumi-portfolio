cfe7f2c100422c963719a3c7dae3c8f8d5732e81
coder6583 <thedude6583@gmail.com>
2024-02-11 03:05:42 -0500

    imported components into home

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,5 +1,20 @@
+import About from "./About/About";
+import Art from "./Art/Art";
+import Blog from "./Blog/Blog";
+import Course from "./Course/Course";
 import styles from "./Home.module.css";
+import News from "./News/News";
+import Sax from "./Sax/Sax";
 
 export default function Home(): JSX.Element {
-  return <div className={styles.parent}></div>;
+  return (
+    <div className={styles.parent}>
+      <Art />
+      <Sax />
+      <Course />
+      <Blog />
+      <News />
+      <About />
+    </div>
+  );
 }
